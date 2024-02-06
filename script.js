bd078bac36d40b5ce7f2f402d69c5489cc6ba2ef
Sheikh <kenneth.sheikh@hotmail.com>
2024-02-06 13:38:54 +0100

    added examples

--- script.js
+++ script.js
@@ -8,3 +8,6 @@ let jsonString = JSON.stringify(JsonBook);
 
 console.log(jsonString);
 // Result:  {"id":1,"isbn":"12345678","cover":"Bear and snow"}
+
+localStorage.setItem("message", "I just stored something in localstorage");
+console.log(localStorage.getItem("message"));

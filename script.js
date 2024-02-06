45102113c9e83fc3544ee099754dcd06fe204713
Sheikh <kenneth.sheikh@hotmail.com>
2024-02-06 14:01:46 +0100

    more examples

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ console.log(jsonString);
 
 localStorage.setItem("message", "I just stored something in localstorage");
 console.log(localStorage.getItem("message"));
+// Result: (Key)Message (Value)I just stored something in localstorage
+
+//Localstorage.clear
+
+let name;
+
+if (4 > 9) {
+  // If condition is true, this block will be executed
+} else {
+  name = "Dean";
+}
+
+console.log(name);
